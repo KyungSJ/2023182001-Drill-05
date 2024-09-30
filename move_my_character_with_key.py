63cf79238a19c97a3bacf2d3125cc64ac798ccb3
KyungSJ <ksjun004@gmail.com>
2024-09-30 13:48:43 +0900

    나의 캐릭터 시트 추가 + 배경 조정하기 + 캐릭터 시트에 맞쳐서 좌표 조정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -2,12 +2,12 @@ from pico2d import *
 
 
 open_canvas()
-grass = load_image('grass.png')
-character = load_image('animation_sheet.png')
+ground = load_image('TUK_GROUND.png')
+character = load_image('my_character.png')
 
 
 def handle_events():
-    global running, dir
+    global running, dir, right_left_move
 
     events = get_events()
     for event in events:
@@ -16,8 +16,10 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir += 1
+                right_left_move = True
             elif event.key == SDLK_LEFT:
                 dir -= 1
+                right_left_move = True
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -27,18 +29,21 @@ def handle_events():
                 dir += 1
 
 running = True
+right_left_move = False
 x = 800 // 2
+y = 600 // 2
 frame = 0
 dir = 0
 
 while running:
     clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    ground.draw(400, 100)
+    character.clip_draw(frame * 60, 180, 60, 60, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
-    x += dir * 5
+    if  right_left_move:
+        x += dir * 5
     delay(0.05)
 
 close_canvas()
